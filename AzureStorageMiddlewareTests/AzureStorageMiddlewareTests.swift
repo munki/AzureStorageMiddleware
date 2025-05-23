5c83eb19d662c81d131a3a9eeb4b7c713421dfc2
Greg Neagle <gregneagle@mac.com>
2025-05-23 15:46:29 -0700

    swiftformat formatting

--- AzureStorageMiddlewareTests/AzureStorageMiddlewareTests.swift
+++ AzureStorageMiddlewareTests/AzureStorageMiddlewareTests.swift
@@ -8,20 +8,17 @@
 import Testing
 
 struct AzureStorageMiddlewareTests {
-    
-
     @Test func buildURLWithSASWithoutLeadingQuestionMark() async throws {
         let url = "https://example.com"
         let sas = "foo=abc123"
         let expectedURL = "https://example.com?foo=abc123"
         #expect(buildAzureStorageURL(url, sas: sas) == expectedURL)
     }
-    
+
     @Test func buildURLWithSASWithLeadingQuestionMark() async throws {
         let url = "https://example.com"
         let sas = "?foo=abc123"
         let expectedURL = "https://example.com?foo=abc123"
         #expect(buildAzureStorageURL(url, sas: sas) == expectedURL)
     }
-
 }
